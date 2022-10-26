3c115e1ce9b3160836467120b5ebae6dd97af620
Nastasia <113934065+Nastasia1505@users.noreply.github.com>
2022-10-26 17:13:25 +0300

    Update index.js
    Add feach

--- index.js
+++ index.js
@@ -2,23 +2,33 @@ const seachInput = document.querySelector('.seach-input');
 const userInfo = document.querySelector('.user-info');
 
 
-function showUserInfo (){
-    if (!seachInput.value){
-        userInfo.style.display = 'none'
-    }
+function showUserInfo(response) {
+  if (!seachInput.value) {
+    userInfo.style.display = 'none'
+  } else {
+    userInfo.style.display = 'flex'
+    userInfo.innerHTML = `<img src="${response.avatar_url}"> 
+      BIO:  ${response.bio}  
+      Followers: ${response.followers}`
+    
+    console.log(response)
+  }
 }
 
 showUserInfo()
 
-let user = seachInput.value;
-seachInput.addEventListener('keyup', () =>{
-console.log('hi')
 
-fetch('https://api.github.com/users/${user}')
-  .then((response) => {
-    return response.json();
-  })
-  .then((data) => {
-    console.log(response);
-  });
+seachInput.addEventListener('keyup', () => {
+  console.log('hi')
+  let user = seachInput.value;
+  fetch(`https://api.github.com/users/${user}`)
+    .then((response) => {
+      return response.json();
+    })
+    .then((response) => {
+      // console.log(response)
+      // console.log(response.bio)
+      showUserInfo(response)
+    })
+
 })
